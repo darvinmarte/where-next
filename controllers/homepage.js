138192310dfa3d2d8e90d9c668f58e1333a81328
oliver23n <oliver.nasevski@gmail.com>
2023-07-13 19:36:48 -0400

    added homeroutes (get all posts, specific post, and login)

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -4,14 +4,58 @@ const authorization = require('../utils/auth');
 
 
 //render all travelposts with a get route with the authentication
-router.get('/',authorization,(req,res) => {
+router.get('/',authorization,async (req,res) => {
     //get the data
-    //serialization
-    //render with appropriate view file
-})
+    try{
+        const travelPostsData = await Travelpost.findAll({
+            include:[
+                {
+                    model: User,
+                    model: Like
+                }
+            ]
+        });
+        //serialization
+        const travelPosts = travelPostsData.map( (travelpost) => travelpost.get({plain: true}));
+        console.log(travelPosts);
+        res.json(travelPosts);
+        //render with appropriate view file
+        // res.render('', {travelPosts})
+
+    }catch(err){
+        console.error(err);
+    }
+});
 
 //render specific travelpost 
 
-// router.get('/', authorization, (req,res) => {
-//     //
-// })
+router.get('/post/:id', authorization, async (req,res) => {
+   try  {
+    //get the data of the travelpost with likes and comments
+    const travelpostData = travelpost.findByPk(req.params.id, {
+        include :[ 
+            {
+                model: User,
+                model: Like,
+                model: Comment 
+            }
+        ]
+    });
+    //serialization
+    const travelPost = travelpostData.get({plain: true});
+    res.json(travelPost);
+    //render to the appropriate view
+    // res.render('', {travelPost}); 
+}catch (err) {
+    console.error(err);
+}
+});
+
+//login route 
+router.get('/login', async (req,res)=> {
+    try{
+    //res.render('login');
+    }catch(err){
+        console.error(err);
+    }
+});
