c50618df96deb28ff27f299bef787b2bcb08fcb7
darvinmarte <119225399+darvinmarte@users.noreply.github.com>
2023-07-13 19:26:01 -0400

    added index.js in models to create relationship

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const user = require('./user');
+const travelPost = require('./travelPost');
+const comment = require('./comment');
+const like = require('./like')
+
+
+
+module.exports = { user, travelPost, comment, like };
