1c00344070a41a848941a636e66099dd6d3de516
Peter McKasty <pmck25@yahoo.com>
2023-07-17 21:40:11 -0400

    added logout function in logout.js in public folder

---
+++ public/logout.js
@@ -0,0 +1,19 @@
+const logout = async () => {
+    try {
+      const response = await fetch('/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log out.');
+      }
+    } catch (error) {
+      console.error('Error during logout:', error);
+      alert('Failed to log out.');
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
