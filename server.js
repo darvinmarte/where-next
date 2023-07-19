184c366bc05e6ae373db43fc14afe78bcb17a7e9
Peter McKasty <pmck25@yahoo.com>
2023-07-18 21:22:28 -0400

    added logic to render comments with partials

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/auth');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({  });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: process.env.secret_sess,
