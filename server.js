85f2bca3cf4db36fc2a41fee3136a0418cd514c4
Noah Salmon <noahsalmon98@gmail.com>
2023-07-20 19:30:04 -0400

    added styling to main.handlebars

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const path = require('path');
-const express = require('express');
+
+const path = require('path'); const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/auth');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({  });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: process.env.secret_sess,
